1f2ee8f7c313b39b7c54ee43d94cebb0c4802bc1
zseaborn63 <zseaborn63@gmail.com>
2016-02-17 14:38:24 -0500

    First extra is working

--- odd_even.py
+++ odd_even.py
@@ -2,7 +2,9 @@
 
 print("Welcome to the Number Program!")
 num = int(input("What is your number!?\n"))
-if num % 2 == 0:
+if num % 4 ==0:
+    print("That number is a multiple of 4!")
+elif num % 2 == 0:
     print("This is an even number!")
 else:
     print("This is an odd number!")
