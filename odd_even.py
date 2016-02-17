384f8d280951afc67d9f8c356a95b149f424a7bd
zseaborn63 <zseaborn63@gmail.com>
2016-02-17 14:36:34 -0500

    Basic program working

--- odd_even.py
+++ odd_even.py
@@ -1,2 +1,8 @@
-print("Hello world")
 
+
+print("Welcome to the Number Program!")
+num = int(input("What is your number!?\n"))
+if num % 2 == 0:
+    print("This is an even number!")
+else:
+    print("This is an odd number!")
